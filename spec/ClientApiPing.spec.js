07e55a13cb702ed56e6cd4a0170e74eec770d749
abir <izatop@gmail.com>
2017-08-15 20:11:22 +0300

    0.2.0

--- spec/ClientApiPing.spec.js
+++ spec/ClientApiPing.spec.js
@@ -15,9 +15,8 @@ async_tape_1.asyncTest('Ping API', (t) => __awaiter(this, void 0, void 0, functi
         publicId: 'empty',
         privateKey: 'empty'
     });
-    const requestId = Math.random().toString();
     const clientApi = service.getClientApi();
-    const response = yield clientApi.ping(requestId);
+    const response = yield clientApi.ping();
     t.ok(/^([a-f0-9]-?)+$/.test(response.getMessage() || ''));
     t.equal(response.isSuccess(), true);
 }));
