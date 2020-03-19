7c505981efa5596a23c7334922ab99dbf1775575
Artur Bier <izatop@gmail.com>
2020-03-19 13:19:44 +0300

    fix: #11

--- src/Client/Response/PayoutClientResponse.ts
+++ src/Client/Response/PayoutClientResponse.ts
@@ -4,6 +4,6 @@ import {ClientResponse} from "../ClientResponse";
 export class PayoutClientResponse<T extends PayoutResponse> extends ClientResponse<T> {
     public isPayoutSuccessResponse(): this is ClientResponse<PayoutSuccessResponse> {
         const {Model} = this.getResponse();
-        return this.isSuccess() && this.has(["TransactionId", "AuthCode"], Model);
+        return this.isSuccess() && PayoutClientResponse.has(["TransactionId", "AuthCode"], Model);
     }
 }
